9b18490a86d8a93a70197010b52adb74cfbdf30d
mike last <pugsyblack@hotmail.com>
2019-01-09 17:00:24 -0800

    add

---
+++ precache-manifest.5a8d6d083a9aaf7385e400fddaaad59c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27734403c48deb2a52a0",
+    "url": "/static/js/main.27734403.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "27734403c48deb2a52a0",
+    "url": "/static/css/main.0b782244.chunk.css"
+  },
+  {
+    "revision": "a7298bc736222dc29d129f6c2ee039a4",
+    "url": "/index.html"
+  }
+];
